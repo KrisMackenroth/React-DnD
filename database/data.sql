5cd3a66afa63055ddc3c14fb4d616b5891fb9b01
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-08-18 00:06:47 +0000

    Created database to hold race, class, and background.

---
+++ database/data.sql
@@ -0,0 +1,6 @@
+insert into "classes" ("class", "hitDie", "equipment")
+values ('Paladin', '1D10', 'Chain mail');
+insert into "races" ("race", "abilityScore", "speed")
+values ('Human', '+1 All', '30 feet');
+insert into "backgrounds" ("background", "skillProficiencies", "languages", "equipment")
+values ('Fisher', 'History, Survival', '+1 Any', 'Fishing tackle, a net, a favorite fishing lure or oiled leather wading boots, a set of traveler"s clothes, and a belt pouch containing 10 gp');
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,28 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."classes" (
+	"class" TEXT NOT NULL,
+	"hitDie" TEXT NOT NULL,
+	"equipment" TEXT NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "public"."races" (
+	"race" TEXT NOT NULL,
+	"abilityScore" TEXT NOT NULL,
+	"speed" TEXT NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "public"."backgrounds" (
+	"background" TEXT NOT NULL,
+	"skillProficiencies" TEXT NOT NULL,
+	"languages" TEXT NOT NULL,
+  "equipment" TEXT NOT NULL
+) WITH (
+  OIDS=FALSE
+);
