36f8458a8eefede6605cfa7e402fa04f3ecc9a0c
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-10-11 02:18:15 +0000

    Added class to StatCalc component in order to use handleClick.

--- client/components/stat-calc.jsx
+++ client/components/stat-calc.jsx
@@ -1,36 +1,64 @@
 import React from 'react';
 
-function StatCalc(props) {
+export default class StatCalc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spells: [],
+      current: ''
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+    const bonus = rollCalc(this.props.stat);
+
+    return (
+      <React.Fragment>
+        <span>
+          {bonus}
+        </span>
+        <a id={bonus} onClick={this.handleClick}> Roll</a>
+
+      </React.Fragment>
+    );
+  }
+}
+
+function rollCalc(props) {
   let bonus;
-  if (props.stat === '1') {
+  if (props === '1') {
     bonus = -5;
-  } else if (props.stat === '3' || props.stat === '2') {
+  } else if (props === '3' || props === '2') {
     bonus = -4;
-  } else if (props.stat === '4' || props.stat === '5') {
+  } else if (props === '4' || props === '5') {
     bonus = -3;
-  } else if (props.stat === '6' || props.stat === '7') {
+  } else if (props === '6' || props === '7') {
     bonus = -2;
-  } else if (props.stat === '8' || props.stat === '9') {
+  } else if (props === '8' || props === '9') {
     bonus = -1;
-  } else if (props.stat === '10' || props.stat === '11') {
+  } else if (props === '10' || props === '11') {
     bonus = 0;
-  } else if (props.stat === '12' || props.stat === '13') {
+  } else if (props === '12' || props === '13') {
     bonus = 1;
-  } else if (props.stat === '14' || props.stat === '15') {
+  } else if (props === '14' || props === '15') {
     bonus = 2;
-  } else if (props.stat === '16' || props.stat === '17') {
+  } else if (props === '16' || props === '17') {
     bonus = 3;
-  } else if (props.stat === '18' || props.stat === '19') {
+  } else if (props === '18' || props === '19') {
     bonus = 4;
-  } else if (props.stat === '20') {
+  } else if (props === '20') {
     bonus = 5;
   }
 
-  return (
-    <span>
-      {bonus}
-    </span>
-  );
+  return bonus;
 }
-
-export default StatCalc;
