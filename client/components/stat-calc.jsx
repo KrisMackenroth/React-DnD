164ac995e74ccdf4a5288567829334a7704f6de0
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-10-11 18:56:23 +0000

    Created function to handle the adding of the bonus to the dice roll.

--- client/components/stat-calc.jsx
+++ client/components/stat-calc.jsx
@@ -5,12 +5,15 @@ export default class StatCalc extends React.Component {
     super(props);
     this.state = {
       spells: [],
-      current: ''
+      current: '',
+      currentRoll: ''
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
+  handleClick(event) {
+    const roll = RollCalc(event.target.id);
+    this.setState({ currentRoll: roll });
 
   }
 
@@ -20,21 +23,33 @@ export default class StatCalc extends React.Component {
 
   render() {
 
-    const bonus = rollCalc(this.props.stat);
+    const bonus = BonusCalc(this.props.stat);
 
     return (
       <React.Fragment>
         <span>
           {bonus}
         </span>
-        <a id={bonus} onClick={this.handleClick}> Roll</a>
-
+        <a id={bonus} className={bonus} onClick={this.handleClick} data-bs-toggle="modal" data-bs-target="#exampleModal"> Roll</a>
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                {this.state.currentRoll}
+              </div>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
 }
 
-function rollCalc(props) {
+function BonusCalc(props) {
   let bonus;
   if (props === '1') {
     bonus = -5;
@@ -61,4 +76,18 @@ function rollCalc(props) {
   }
 
   return bonus;
+}
+
+function RollCalc(props) {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  const roll = getRandomInt(1, 21);
+
+  const final = roll + parseInt(props);
+
+  return final;
 }
