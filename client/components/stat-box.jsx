7bcae558d5380fcb08df004902c2f7b0dbe3847c
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-10-24 19:58:20 +0000

    Made numbers be an input on stat page to allow for editable numbers.

--- client/components/stat-box.jsx
+++ client/components/stat-box.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 
-function StatBox(props) {
-  return (
-    <div className="stat-block">
-      <div className="stat-text mb-1">{props.name}</div>
-      <div className='stat-box mb-1'>{props.stat}</div>
-      <div className='border-box mb-1'>{props.bonus}</div>
-    </div>
+// function StatBox(props) {
+//   return (
+//     <div className="stat-block">
+//       <div className="stat-text mb-1">{props.name}</div>
+//       <div className='stat-box mb-1'>{props.stat}</div>
+//       <div className='border-box mb-1'>{props.bonus}</div>
+//     </div>
 
-  );
+//   );
+// }
+
+// export default StatBox;
+
+export default class StatBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentRoll: '',
+      proficent: 'not-proficent',
+      editModeEnabled: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({ editModeEnabled: !this.state.editModeEnabled });
+  }
+
+  render() {
+
+    return (
+      <div className="stat-block">
+        <div className="stat-text mb-1">{this.props.name}</div>
+        <input type="number" value={this.props.stat} className='stat-box mb-1 number' disabled={!this.state.editModeEnabled}></input>
+        <div className='border-box mb-1'>{this.props.bonus}</div>
+        <a role="button" title="Edit" onClick={this.handleClick}>✏️</a>
+      </div>
+    );
+  }
 }
-
-export default StatBox;
