d5b253a46201e8b0b45c14e7a66cef7f5bba20e9
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-10-26 19:03:09 +0000

    User can change stats, but only one stat at a time.

--- client/components/stat-box.jsx
+++ client/components/stat-box.jsx
@@ -19,7 +19,17 @@ export default class StatBox extends React.Component {
     this.state = {
       currentRoll: '',
       proficent: 'not-proficent',
-      editModeEnabled: false
+      editModeEnabled: false,
+      stat: [],
+      character: [],
+      test: [],
+      level: '',
+      str: '',
+      dex: '',
+      con: '',
+      wis: '',
+      int: '',
+      cha: ''
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -30,14 +40,61 @@ export default class StatBox extends React.Component {
   }
 
   handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    const info = {
+      str: this.state.str,
+      dex: this.state.dex,
+      con: this.state.con,
+      wis: this.state.wis,
+      int: this.state.int,
+      cha: this.state.cha,
+      characterId: this.props.characterId
+    };
+    const req = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(info)
+    };
+    fetch(`/api/stats/${info.characterId}`, req);
   }
 
+  componentDidMount() {
+    fetch(`/api/characters/${this.props.characterId}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ str: data[0].str });
+        this.setState({ dex: data[0].dex });
+        this.setState({ con: data[0].con });
+        this.setState({ wis: data[0].wis });
+        this.setState({ cha: data[0].cha });
+        this.setState({ int: data[0].int });
+        this.setState({ test: data[0].prof });
+        this.setState({ level: data[0].level });
+        this.setState({ character: data[0] });
+      }
+      );
+    fetch('/api/races')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ races: data });
+      }
+      );
+
+  }
+
+  // componentDidMount() {
+  //   const test = this.props.stat;
+  //   this.setState({ stat: test });
+  // }
+
   render() {
-    const temp = this.props.stat.toString();
     return (
       <div className="stat-block">
         <div className="stat-text mb-1">{this.props.name}</div>
-        <input onChange={this.handleChange} type="number" defaultValue={temp} className='stat-box mb-1 number'></input>
+        <input name={this.props.name} onChange={this.handleChange} type="number" defaultValue={this.state.character[this.props.name]} className='stat-box mb-1 number'></input>
         <div className='border-box mb-1'>{this.props.bonus}</div>
       </div>
     );
--- client/pages/character-details.jsx
+++ client/pages/character-details.jsx
@@ -100,22 +100,22 @@ export default class CharacterDetails extends React.Component {
         <div className='row justify-content-center'>
           <div className='col-4'>
             <div className='row'>
-              <div className='col d-flex justify-content-center'><StatBox name="Strength" stat={strength} bonus={strengthBonus} /></div>
+              <div className='col d-flex justify-content-center'><StatBox tempName='newStr' characterId={this.props.characterId} name="str" stat={strength} bonus={strengthBonus} /></div>
             </div>
             <div className='row'>
-              <div className='col d-flex justify-content-center'><StatBox name="Dexterity" stat={dexterity} bonus={dexBonus} /></div>
+              <div className='col d-flex justify-content-center'><StatBox characterId={this.props.characterId} name="dex" stat={dexterity} bonus={dexBonus} /></div>
             </div>
             <div className='row'>
-              <div className='col d-flex justify-content-center'><StatBox name="Constitution" stat={constitution} bonus={conBonus} /></div>
+              <div className='col d-flex justify-content-center'><StatBox characterId={this.props.characterId} name="con" stat={constitution} bonus={conBonus} /></div>
             </div>
             <div className='row'>
-              <div className='col d-flex justify-content-center'><StatBox name="Wisdom" stat={wisdom} bonus={wisBonus} /></div>
+              <div className='col d-flex justify-content-center'><StatBox characterId={this.props.characterId} name="wis" stat={wisdom} bonus={wisBonus} /></div>
             </div>
             <div className='row'>
-              <div className='col d-flex justify-content-center'><StatBox name="Intelligence" stat={intelligence} bonus={intBonus} /></div>
+              <div className='col d-flex justify-content-center'><StatBox characterId={this.props.characterId} name="int" stat={intelligence} bonus={intBonus} /></div>
             </div>
             <div className='row'>
-              <div className='col d-flex justify-content-center'><StatBox name="Charisma" stat={charisma} bonus={chaBonus} /></div>
+              <div className='col d-flex justify-content-center'><StatBox characterId={this.props.characterId} name="cha" stat={charisma} bonus={chaBonus} /></div>
             </div>
           </div>
           <div className='col-4'>
